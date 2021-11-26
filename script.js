02aa2fb2f16e9973f83a30f4115b25018f8d1681
Andrey Shmykov <feomatar4eg@gmail.com>
2021-11-27 01:15:20 +0300

    выполнил первый урок

---
+++ script.js
@@ -0,0 +1,11 @@
+let title = "Заголовок";
+let screens = 5;
+let screenPrice = 333;
+let rollback = "rollback";
+let fullPrice = screens * screenPrice;
+let adaptive = true;
+
+
+alert("Цена за все экраны: " + fullPrice);
+console.log(title);
+
