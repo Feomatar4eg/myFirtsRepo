515497dbf1cbda3f608888302c3c07c8f12e4a27
Andrey Shmykov <feomatar4eg@gmail.com>
2021-11-27 01:47:35 +0300

    начал второй урок

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = "Калькулятор цены работы",
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 4,
+    rollback = 80,
+    fullPrice = 500,
+    adaptive = true;
 
 
-alert("Yahoo!");
-console.log("logloglog");
+console.log(typeof(title), typeof(fullPrice), typeof(adaptive));
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log(screens.toLowerCase());
+console.log(fullPrice * (rollback / 100));
+
 
