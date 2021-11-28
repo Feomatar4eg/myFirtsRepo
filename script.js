dd057654dada9e369b9ed98961b7d1ae36163830
Andrey Shmykov <feomatar4eg@gmail.com>
2021-11-28 17:58:28 +0300

    простая задача решено

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ let title = "Калькулятор цены работы",
     adaptive = true;
 
 
-console.log(typeof(title), typeof(fullPrice), typeof(adaptive));
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
 console.log(screens.length);
+
 console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log(screens.toLowerCase());
+console.log('Стоимость разработки сайта '+ fullPrice + ' рублей');
+
+screens = screens.toLowerCase().replaceAll(' ','').split(',');
+
+console.log(screens);
 console.log(fullPrice * (rollback / 100));
 
 
