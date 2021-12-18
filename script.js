7fa818eab047445694e59a2cc01229064c4f321d
Feomatar4eg <feomatar4eg@gmail.com>
2021-12-18 21:23:12 +0300

    hard lesson07

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let week = ['sun','mon','tue','wed','thu','fri','sat'];
+
+let now = new Date()
+
+for(let key in week){
+    if (key == now.getDay()) {
+        console.log(week[key].bold());
+    }else if (key == 0 || key == 6){
+       console.log(week[key].italics());
+    }else console.log(week[key]);
+}
